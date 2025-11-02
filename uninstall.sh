cf5595d554c98483cbfb8ed0ad1dac285820f9dd
Alen <expspeedrunner@gmail.com>
2025-11-02 14:00:37 -0500

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-rm -rf "$HOME/.local/share/neo" "$HOME/.local/state/neo" "$HOME/.config/neo"
-rm -f "$HOME/.local/bin/neo" "$HOME/.local/bin/neo.sh"
-hash -r 2>/dev/null || true
-echo "neo removed."
