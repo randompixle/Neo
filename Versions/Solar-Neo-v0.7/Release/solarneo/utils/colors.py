05a21d8e102eb8529d1c05844703cf822116b400
Alen <expspeedrunner@gmail.com>
2025-11-02 20:27:52 -0500

    Delete Versions/Solar-Neo-v0.7/Release directory

--- Versions/Solar-Neo-v0.7/Release/solarneo/utils/colors.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def _c(code): return f"\033[{code}m" if sys.stdout.isatty() else ""
-def info(msg): print(f"{_c('36')}ℹ{_c('0')} {msg}")
-def ok(msg):   print(f"{_c('32')}✔{_c('0')} {msg}")
-def warn(msg): print(f"{_c('33')}⚠{_c('0')} {msg}")
-def err(msg):  print(f"{_c('31')}✖{_c('0')} {msg}")
